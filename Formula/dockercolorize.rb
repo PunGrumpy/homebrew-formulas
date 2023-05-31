ccd0c213a1d61f03423ca394c6d115f902b26659
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-31 14:33:39 +0700

    fix: audit

--- Formula/dockercolorize.rb
+++ Formula/dockercolorize.rb
@@ -2,7 +2,7 @@ class Dockercolorize < Formula
   desc "Enhancing Docker output with vibrant colors"
   homepage "https://github.com/PunGrumpy/dockercolorize"
   version "2.4.7"
-  
+
   if OS.mac?
     url "https://github.com/PunGrumpy/dockercolorize/releases/download/#{version}/dockercolorize-darwin-amd64"
     sha256 "2df82524b7d7e90b1c2df7656537c7fe61f4746f4d4c2620d92d897421da9c80"
@@ -13,13 +13,13 @@ class Dockercolorize < Formula
 
   def install
     if OS.mac?
-        bin.install "dockercolorize-darwin-amd64" => "dockercolorize"
+      bin.install "dockercolorize-darwin-amd64" => "dockercolorize"
     else
-        bin.install "dockercolorize-linux-amd64" => "dockercolorize"
+      bin.install "dockercolorize-linux-amd64" => "dockercolorize"
     end
   end
-  
+
   test do
     assert_match "Usage:", shell_output("#{bin}/dockercolorize --help")
   end
-end  
+end
