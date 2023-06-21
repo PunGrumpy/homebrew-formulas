39b00c1643735a283ec11a953c5e640746911aa2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-06-21 15:30:05 +0700

    feat(dockercolorize): update new file

--- Formula/dockercolorize.rb
+++ Formula/dockercolorize.rb
@@ -5,10 +5,10 @@ class Dockercolorize < Formula
 
   if OS.mac?
     url "https://github.com/PunGrumpy/dockercolorize/releases/download/#{version}/dockercolorize-darwin-amd64"
-    sha256 "d348bf5efc1824529683f295792066c1ba4caf9c6053a75c13fcb0f66763b2dc"
+    sha256 "39efaebbbb23eecc792b0599929532632049f69949415bf9f7a9c3949b8d5690"
   else
     url "https://github.com/PunGrumpy/dockercolorize/releases/download/#{version}/dockercolorize-linux-amd64"
-    sha256 "68ea964537167548917895f42dbcd8c5d4ffdd380515046c8418ce8ce64d5261"
+    sha256 "f467adebb6491262e703fafd8637f6c04e0e07ba0eca4d3dc0de818c534fdc9b"
   end
 
   def install
