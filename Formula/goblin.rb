1ef428f8015ccc7f5da88c62a9c1f1d6b9737e9c
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-04 18:24:51 +0700

    feat(goblin): update new version `0.1.6`

--- Formula/goblin.rb
+++ Formula/goblin.rb
@@ -1,14 +1,14 @@
 class Goblin < Formula
-  desc "Command-line tool for hashing and reversing hashes using Jenkins"
+  desc "Command-line tool for hashing and reversing hashes using Jenkins' one time hash"
   homepage "https://github.com/PunGrumpy/goblin"
-  version "0.1.4"
+  version "0.1.6"
 
   if OS.mac?
     url "https://github.com/PunGrumpy/goblin/releases/download/#{version}/goblin-darwin-amd64"
-    sha256 "306706f1531e600cf065104936dadd53595f9c6dd4e285c1b15bc1f1c1df0a9c"
+    sha256 "ff418a6956a999798a004264eca03a72e749a81458d5d2fef5f0881b87ae6ba5"
   else
     url "https://github.com/PunGrumpy/goblin/releases/download/#{version}/goblin-linux-amd64"
-    sha256 "f65dc8e77804696c6961731868f286d0d71d360534cb907c8f4658cce78ec187"
+    sha256 "61a5045f44ff2816f8607b269f171f591388802d5c19c9aa3382b6182e082e81"
   end
 
   def install
